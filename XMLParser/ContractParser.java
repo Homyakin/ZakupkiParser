40ce9ad390bdd094551c9a2bafdbaf1120b2c44c
Homyakin <tox.home@yandex.ru>
2019-07-21 23:53:50 +0300

    Update ContractParser.java

--- XMLParser/ContractParser.java
+++ XMLParser/ContractParser.java
@@ -6,12 +6,11 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
 import DocumentsInfo.ContractInfo;
+import DocumentsInfo.Currency;
 import DocumentsInfo.CustomerInfo;
 import DocumentsInfo.PurchaseTypeInfo;
 import DocumentsInfo.SupplierInfo;
@@ -52,11 +51,12 @@ public class ContractParser
 	}
 	
 	public PurchaseTypeInfo parsePurchaseTypeInfo() throws XMLStreamException {
-		String code = null, name = null;
+		Integer code = null; 
+		String name = null;
 		PurchaseTypeInfo purchaseType = null;
 		while(processor.getNextInBlock("purchaseTypeInfo")) {
 			if("code".equals(processor.getName()))
-				code = processor.getText();
+				code = Integer.parseInt(processor.getText());
 			else if("name".equals(processor.getName()))
 				name = processor.getText();
 		}
@@ -96,15 +96,41 @@ public class ContractParser
 		supplier.setINN(INN);
 		return supplier;
 	}
-	public ContractInfo parse() throws XMLStreamException, IOException {
+	
+	public Currency parseCurrency() throws XMLStreamException {
+		Currency currency = null;
+		String letterCode = null, code = null, digitalCode = null,
+				name = null;
+		while(processor.getNextInBlock("currency")) {
+			if("letterCode".equals(processor.getName())) {
+				letterCode = processor.getText();
+			} else if("code".equals(processor.getName())) {
+				code = processor.getText();
+			} else if("digitalCode".equals(processor.getName())) {
+				digitalCode = processor.getText();
+			} else if("name".equals(processor.getName())) {
+				name = processor.getText();
+			} else {
+				processor.skipBlock();
+			}
+		}
+		currency = new Currency(name);
+		currency.setCode(code);
+		currency.setDigitalCode(digitalCode);
+		currency.setLetterCode(letterCode);
+		return currency;
+	}
+	
+	public ContractInfo parseContract() throws XMLStreamException, IOException {
 		ContractInfo contract = null;
 		processor.findStartBlock("contractData");
-		String GUID = null;
+		String GUID = null, price = null, rubPrice = null;
 		LocalDateTime createDateTime = null;
 		CustomerInfo customer = null;
 		SupplierInfo supplier = null;
-		LocalDate contractDate = null;
+		LocalDate contractDate = null, startExecutionDate = null, endExecutionDate = null;
 		PurchaseTypeInfo purchaseType = null;
+		Currency currency = null;
 		processor.findStartBlock("price");
 		System.out.println(processor.getName());
 		System.out.println(processor.getText());
@@ -126,21 +152,24 @@ public class ContractParser
 				supplier = parseSupplier();
 			}else if("purchaseTypeInfo".equals(processor.getName())) {
 				purchaseType = parsePurchaseTypeInfo();
-			}else if("price".equals(processor.getName()))
-			{
-				System.out.println(processor.getName());
-				System.out.println(processor.getText());
-			}else if("currency".equals(processor.getName()))
-			{
-				System.out.println(processor.getName());
-				//processor.findBlock("name");
-				System.out.println(processor.getText());
+			}else if("price".equals(processor.getName())) {
+				price = processor.getText();
+			} else if("rubPrice".equals(processor.getName())) { 
+				rubPrice = processor.getText();
+			} else if("currency".equals(processor.getName())) {
+				currency = parseCurrency();
+			} else if("startExecutionDate".equals(processor.getName())) {
+				startExecutionDate = LocalDate.parse(processor.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
+			} else if("endExecutionDate".equals(processor.getName())) {
+				endExecutionDate = LocalDate.parse(processor.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
+			} else if("contractPositions".equals(processor.getName())) {
+				//TODO
 			} else {
 				processor.skipBlock();
 			}
 		}
 		
-		
+		contract = new ContractInfo(GUID, createDateTime, customer, contractDate, purchaseType, price, currency);
 		return contract;
 	}
 }
