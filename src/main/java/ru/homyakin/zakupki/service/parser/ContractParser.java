4229ce41cd965deed07812bcc5984992ffc53410
Homyakin <tox.home@ya.ru>
2019-12-18 17:44:48 +0300

    [RF] added logs for parser

--- src/main/java/ru/homyakin/zakupki/service/parser/ContractParser.java
+++ src/main/java/ru/homyakin/zakupki/service/parser/ContractParser.java
@@ -1,17 +1,21 @@
 package ru.homyakin.zakupki.service.parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.homyakin.zakupki.documentsinfo.ContractInfo;
 import ru.homyakin.zakupki.documentsinfo.DocumentInfo;
 import ru.homyakin.zakupki.documentsinfo._223fz.contract.Contract;
 import ru.homyakin.zakupki.service.parser.interfaces.DocumentParser;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.IOException;
 
 public class ContractParser implements DocumentParser {
+    private final static Logger logger = LoggerFactory.getLogger(ContractParser.class);
 
     @Override
     public DocumentInfo parse(String filePath) {
@@ -19,8 +23,9 @@ public class ContractParser implements DocumentParser {
             JAXBContext jc = JAXBContext.newInstance(Contract.class);
             Unmarshaller unmarshaller = jc.createUnmarshaller();
             return new ContractInfo((Contract) unmarshaller.unmarshal(new File(filePath)));
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (JAXBException e){
+            logger.error("Unable to parse {}", filePath);
+            //TODO add exception
         }
         return null;
     }
