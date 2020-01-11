2734b0bc174d799533afbeb1ac5856a1eca620b6
Homyakin <tox.home@ya.ru>
2020-01-11 17:32:59 +0300

    [NF] added okved2

--- open_data/insert_all.py
+++ open_data/insert_all.py
@@ -4,6 +4,7 @@ import insert_okei
 import insert_okopf
 import insert_okpd2
 import insert_okved
+import insert_okved2
 import insert_plan_item_status
 import insert_plan_status
 import insert_purchase_category
---
+++ open_data/insert_okved2.py
@@ -0,0 +1,7 @@
+import insert
+
+table = 'okved2'
+columns = ['code', 'name']
+json_fields = ['Kod', 'Name']
+
+insert.insert(table, columns, json_fields)
