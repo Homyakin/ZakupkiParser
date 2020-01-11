6e7d535e006cd79afed8801d9b2a26e2ee70df12
Homyakin <tox.home@ya.ru>
2020-01-11 17:03:39 +0300

    [NF] added okpd2

--- open_data/insert_all.py
+++ open_data/insert_all.py
@@ -2,6 +2,7 @@ import insert_currency
 import insert_okdp
 import insert_okei
 import insert_okopf
+import insert_okpd2
 import insert_plan_item_status
 import insert_plan_status
 import insert_purchase_category
---
+++ open_data/insert_okpd2.py
@@ -0,0 +1,7 @@
+import insert
+
+table = 'okpd2'
+columns = ['code', 'name']
+json_fields = ['Kod', 'Name']
+
+insert.insert(table, columns, json_fields)
