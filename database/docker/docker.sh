2fe4c1107d8db61949096f8d51d9fc2744a459b8
Homyakin <tox.home@ya.ru>
2020-01-22 21:13:52 +0300

    [NF] united docker launch

--- database/docker/docker.sh
+++ database/docker/docker.sh
@@ -1,6 +1,4 @@
 docker stop zakupki || true \
 && docker rm zakupki || true \
 && docker build -f Dockerfile -t zakupki_img . \
-&& docker run --name zakupki -p 3306:3306 -e MYSQL_ROOT_PASSWORD=12345 -v $(pwd)/db_zakupki:/var/lib/mysql -d zakupki_img \
-&& sleep 10 \
-&& docker exec -i zakupki mysql -uroot -p12345 < sql/script.sql
+&& docker run --name zakupki -p 3306:3306  -v $(pwd)/db_zakupki:/var/lib/mysql -d zakupki_img
