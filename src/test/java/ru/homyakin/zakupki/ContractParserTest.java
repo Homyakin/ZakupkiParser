cd2d3373398a061995f4866e02dff9f3d53c85a3
Homyakin <tox.home@ya.ru>
2021-11-28 13:37:34 +0300

    [RF] remove unused imports

--- src/test/java/ru/homyakin/zakupki/ContractParserTest.java
+++ src/test/java/ru/homyakin/zakupki/ContractParserTest.java
@@ -1,22 +1,11 @@
 package ru.homyakin.zakupki;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import ru.homyakin.zakupki.models.ParseFile;
 import ru.homyakin.zakupki.models._223fz.contract.Contract;
 import ru.homyakin.zakupki.service.parser.ContractParser;
-import ru.homyakin.zakupki.service.parser.MainXmlParser;
-import ru.homyakin.zakupki.service.storage.ParseFileQueue;
-import ru.homyakin.zakupki.service.storage.Queue;
 import ru.homyakin.zakupki.utils.TestUtils;
 
 
